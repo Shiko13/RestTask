34c3cdaa7826fd78c91c5e6de3d77448fec89e10
Sergey <shikofreid@yandex.ru>
2023-11-21 19:30:28 +0400

    Removed unnecessary fields

--- src/main/java/com/epam/model/dto/TraineeForTrainerDtoOutput.java
+++ src/main/java/com/epam/model/dto/TraineeForTrainerDtoOutput.java
@@ -3,13 +3,9 @@ package com.epam.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class TraineeForTrainerDtoOutput {
 
